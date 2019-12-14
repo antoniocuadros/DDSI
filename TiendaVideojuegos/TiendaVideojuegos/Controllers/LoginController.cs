ff7247aa70f323be900683219fd810ca8f789e85
albertooc98 <albertooc@correo.ugr.es>
2019-12-14 21:01:29 +0100

    login view created

---
+++ TiendaVideojuegos/TiendaVideojuegos/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TiendaVideojuegos.Data;
+
+namespace TiendaVideojuegos.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly AbonadosContext _context;
+
+        public LoginController(AbonadosContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
