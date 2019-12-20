4af5fb197d87e8ee6c3eb52571d046dd7921d81c
antoniocuadros <antculap@gmail.com>
2019-12-20 02:43:46 +0100

    BuenasVistas2

--- TiendaVideojuegos/TiendaVideojuegos/Controllers/HomeController.cs
+++ TiendaVideojuegos/TiendaVideojuegos/Controllers/HomeController.cs
@@ -54,5 +54,10 @@ namespace TiendaVideojuegos.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult SuperMario()
+        {
+            return View();
+        }
     }
 }
