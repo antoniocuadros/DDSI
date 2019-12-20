c6f59403a45dded9242aaeca6cb057dc9b92bb89
albertooc98 <albertooc@correo.ugr.es>
2019-12-20 05:38:43 +0100

    final

---
+++ TiendaVideojuegos/TiendaVideojuegos/ViewModels/DevolverViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TiendaVideojuegos.Models;
+
+namespace TiendaVideojuegos.ViewModels
+{
+    public class DevolverViewModel
+    {
+        public List<Ventas> ventas { get; set; }
+        public string nombreProducto { get; set; }
+        public float PrecioProducto { get; set; }
+
+    }
+}
